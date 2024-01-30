7e059c3bb47838552d791724918b9dac3485440f
Fabian Gut <fabian.gut@ethy.ch>
2024-01-30 17:36:46 +0100

    feat: initial project setup

---
+++ src/test/java/ch/ethy/recipe/RecipeApplicationTests.java
@@ -0,0 +1,12 @@
+/* (C)2024 */
+package ch.ethy.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
