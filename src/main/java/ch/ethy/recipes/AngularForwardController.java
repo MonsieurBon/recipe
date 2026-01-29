f23af955e28518951b94880b7c2a4b4b96fbe52e
Fabian Gut <fabian.gut@ethy.ch>
2026-01-29 07:55:52 +0100

    feat: load fonts locally

--- src/main/java/ch/ethy/recipes/AngularForwardController.java
+++ src/main/java/ch/ethy/recipes/AngularForwardController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AngularForwardController {
-  @GetMapping("{path:^(?!api|public|swagger)[^\\.]*}/**")
+  @GetMapping("{path:^(?!api|media|assets|swagger)[^\\.]*}/**")
   public String handleForward() {
     return "forward:/";
   }
