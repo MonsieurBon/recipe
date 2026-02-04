191651bd9efa0a26de5e06bc9a812fcf69ce9790
Fabian Gut <fabian.gut@ethy.ch>
2026-02-04 18:03:16 +0100

    feat: add toolbar to top of UI

--- src/main/webapp/app/app.ts
+++ src/main/webapp/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatToolbar, MatIcon, MatIconButton, MatMenu, MatMenuTrigger, MatMenuItem, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('recipes');
-}
+export class App {}
